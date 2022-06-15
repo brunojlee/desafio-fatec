fdaebb574d8c423884b85e1ae4ee30d82291af54
Bruno Roja <brunojlee@hotmail.com>
2022-06-15 18:24:09 -0400

    feat: :sparkles: Login page creation

---
+++ src/components/LoginCard/LoginCard.js
@@ -0,0 +1,87 @@
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import FeedbackContext from '../../context/FeedbackContext';
+import './LoginCard.css';
+
+function LoginCard() {
+  const navigate = useNavigate();
+  const {
+    userEmail,
+    setUserEmail,
+    password,
+    setPassword,
+    setLoading,
+    isButtonDisabled,
+    setIsButtonDisabled
+  } = useContext(FeedbackContext);
+
+  const handleEmail = ({ target: { value } }) => {
+    setUserEmail(value);
+  };
+
+  const handlePassword = ({ target: { value } }) => {
+    setPassword(value);
+  };
+
+  const handleClick = () => {
+    localStorage.setItem('user', JSON.stringify({ email: userEmail }));
+    setLoading(true);
+    navigate('/feedback');
+  };
+
+  useEffect(() => {
+    const validateButton = async () => {
+      const minPasswordLength = 6;
+      const emailFormatRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+      if (password.length > minPasswordLength && userEmail.match(emailFormatRegex)) {
+        await setIsButtonDisabled(false);
+      } else {
+        await setIsButtonDisabled(true);
+      }
+    };
+    validateButton();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userEmail, password]);
+
+  return (
+    <div className="login_container">
+      <img
+        className="login_image"
+        src={require('../../imgs/logo.png')}
+        alt="logo app"
+      />
+      <form className="input_container">
+        <label className="email_label" htmlFor="email">
+          <input
+            className="email_input"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={ userEmail }
+            onChange={ (event) => handleEmail(event) }
+          />
+        </label>
+        <label className="password_label" htmlFor="password">
+          <input
+            className="password_input"
+            type="password"
+            name="password"
+            placeholder="Senha"
+            value={ password }
+            onChange={ (event) => handlePassword(event) }
+          />
+        </label>
+        <button
+          className="login_button"
+          disabled={ isButtonDisabled }
+          type="button"
+          onClick={ () => handleClick() }
+        >
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginCard;
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginCard from '../../components/LoginCard/LoginCard'
+import './Login.css'
+
+export default function Login() {
+  return (
+    <div className='login_page'>
+      <LoginCard />
+    </div>
+  )
+}
